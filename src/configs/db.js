3ed28ceb4c1311921eb7bece71b4d2785b74b76a
muhlis <muhlis.habibpradana@altomatik.com>
2020-02-06 15:01:03 +0700

    update

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-const mysql = require('mysql');
+require("dotenv").config();
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'remotemysql.com',
-    user: process.env.DB_USER || 'n7WMuGoxl9',
-    password: process.env.DB_PASS || 'iNXYG2M1SQ',
-    database: process.env.DB_NAME || 'n7WMuGoxl9',
+  host: process.env.DB_HOST || "remotemysql.com",
+  user: process.env.DB_USER || "n7WMuGoxl9",
+  password: process.env.DB_PASS || "iNXYG2M1SQ",
+  database: process.env.DB_NAME || "n7WMuGoxl9"
 });
 
-connection.connect((err) => {
-    if (err) console.log(`Error: ${err}`);
+connection.connect(err => {
+  if (err) console.log(`Error: ${err}`);
 });
 
 module.exports = connection;
